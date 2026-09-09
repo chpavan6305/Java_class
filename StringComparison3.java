b434792205fbf1fd132fa36fda9976a6f0fb3a79
CHEEDELLA PAVAN KUMAR <chpavan6305@gmail.com>
2026-09-09 19:23:54 +0530

    Add files via upload
    String inbuilt methods

---
+++ StringComparison3.java
@@ -0,0 +1,11 @@
+package org.jsp;
+
+public class StringComparison3 {
+    public static void main(String[] args) {
+
+        String s = "   Ja   va   ";
+
+        System.out.println(s);
+        System.out.println(s.trim());
+    }
+}
